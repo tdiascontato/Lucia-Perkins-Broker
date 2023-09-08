9d145ae0ebf30219c8d4505903f406ea190c8d1b
Tiago Dias <98658691+tdiascontato@users.noreply.github.com>
2023-09-08 15:28:23 -0300

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,11 +1,31 @@
-<?php include 'partials/header.php'; ?>
+<?php 
+    session_start();
+    include 'partials/header.php'; 
+?>
 
 <!--Section-->
 <section class="landing_page">
-
+<div class="hello_contact">
+    <img src="<?=ROOT_URL?>img/creci.png" alt"Image">
+    <div class="beliche">
+        <h1>Venha ser assessorado!</h1>
+        <div class="section_contact">
+        <label for="email_send">Digite abaixo seu email ou telefone que te retornamos!</label>
+            <form action="<?=ROOT_URL?>send_email.php" method="POST">
+                <div class="contact_contacts">
+                    <input type="text" name="email_send" id="email_send">
+                    <button type="submit" name="submit" class="btn_send">SEND!</button>
+                </div>
+                <a href="https://wa.me/5521990032507?text=Gostaria+de+informa%C3%A7%C3%B5es+de+um+im%C3%B3vel%21" class="message_whatsapp" target="_blank">
+                    Me chame no whatsapp!
+                </a>
+            </form>
+        </div>
+    </div>
+</div>
 <div class="vantagens">
-    <div class="card_vantagens">
-        <h2>Consultas e Aconselhamentos</h2>
+    <div class="card_vantagens wine top">
+        <h2>Consultas</h2>
         <img src="<?= ROOT_URL ?>img/consulta.jpeg" alt="Consultas">
     </div>
     <div class="card_vantagens">
@@ -27,59 +47,61 @@
             <button type="submit" name="submit" class="btn_send">SEND!</button>
         </div>
         <a href="https://wa.me/5521990032507?text=Gostaria+de+informa%C3%A7%C3%B5es+de+um+im%C3%B3vel%21" class="message_whatsapp" target="_blank">
-        Clique e me chame no whatsapp!
+            Clique e me chame no whatsapp!
         </a>
     </form>
 </div>
 <!--Contact_Section-->
 
+<!--feedbacks-->
+
+<div class="feedbacks">
+    <div class="feed">
+            <h2 class="feed-title">Ótima Experiência</h2>
+            <p class="feed-description">Fez toda a diferença na busca pela nossa casinha. Teve muita paciência e dedicação. Recomendamos!</p>
+        </div>
+        <div class="feed none">
+            <h2 class="feed-title">Muito Dedicados</h2>
+            <p class="feed-description">Confiamos na equipe e deu tudo certo. Eles foram pacientes e fomos vendo com calma exatamente o que queríamos. Serviço de alta qualidade.</p>
+        </div>
+        <div class="feed">
+            <h2 class="feed-title">Atenciosos e Experientes</h2>
+            <p class="feed-description">Foi uma experiência muito boa com a equipe. Sempre acompanhando cada passo e com interações das etapas.</p>
+        </div>
+</div>
+
+<!--feedbacks-->
+
 <!-- Imóveis Novo Usado -->
+<h1 class="title">Atendemos todas as categorias!</h1>
 <div class="novo_usado">
-    <div class="card_novo_usado">
+    <div class="card_vantagens">
         <h2>Imóveis Prontos</h2>
         <img src="<?= ROOT_URL ?>img/pronto.jpeg" alt="Consultas">
     </div>
-    <div class="card_novo_usado">
+    <div class="card_vantagens meio">
         <h2>Imóveis Lançamentos</h2>
         <img src="<?= ROOT_URL ?>img/lancamento.png" alt="Simulacao de Financiamento">
     </div>
 </div>
-<!-- Imóveis -->
-
-<!-- Imóveis Casa Apart Rural-->
-<div class="imoveis_condicao">
-    <div class="card_imoveis_condicao">
-        <h2>Casas</h2>
-        <img src="<?= ROOT_URL ?>img/casa.jpeg" alt="Consultas">
-    </div>
-    <div class="card_imoveis_condicao">
-        <h2>Apartamento</h2>
-        <img src="<?= ROOT_URL ?>img/apartamento.jpeg" alt="Simulacao de Financiamento">
-    </div>
-    <div class="card_imoveis_condicao">
-        <h2>Rural</h2>
-        <img src="<?= ROOT_URL ?>img/rural.jpeg" alt="Simulacao de Financiamento">
-    </div>
-</div>
-<!-- Imóveis -->
 
 <!--Contact_section-->
 <div class="section_contact">
-    <label for="email_send">Digite abaixo seu email ou telefone que eu te retorno!</label>
+    <label for="email_send">Digite abaixo seu email ou telefone que te retornamos!</label>
     <form action="<?=ROOT_URL?>send_email.php" method="POST">
         <div class="contact_contacts">
             <input type="text" name="email_send" id="email_send">
             <button type="submit" name="submit" class="btn_send">SEND!</button>
         </div>
         <a href="https://wa.me/5521990032507?text=Gostaria+de+informa%C3%A7%C3%B5es+de+um+im%C3%B3vel%21" class="message_whatsapp" target="_blank">
-        Clique e me chame no whatsapp!
+        Me chame no whatsapp!
         </a>
     </form>
 </div>
 <!--Contact_Section-->
 
-<div class="contact_icons">
-    <div class="contact_icons_socials">
+<div class="hello_contact_two">
+    <div class="beliche">
         <img src="./img/whatsapp.png" alt="socials media" class="img_contacts">
         <a href="" target="_blank" class="contact_links whats">Meu whatsapp!</a>
         <img src="./img/facebook.png" alt="socials media" class="img_contacts">
