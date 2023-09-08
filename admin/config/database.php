314a85e54e44d9f975cb0024a71a00fdec783871
Tiago Dias <98658691+tdiascontato@users.noreply.github.com>
2023-09-08 15:56:01 -0300

    Update database.php

--- admin/config/database.php
+++ admin/config/database.php
@@ -1,8 +1,7 @@
 <?php
-    require 'constants.php';//Mudei de 'config/' -> ''
+    require 'constants.php';
     //connection com db
     $connection = new mysqli(db_host, db_username, db_password, db_name);
     if(mysqli_errno($connection)){
         die(mysqli_error($connection));
     }
-?> 
